5d732c7f6d17cdb8b3d94388d978b7a1929ec1d4
Ismaellucas-BR <ismaellucas36@gmail.com>
2025-08-18 16:19:10 -0300

    Continuando pagina interna dos agentes

--- src/pages/InternalAgents.tsx
+++ src/pages/InternalAgents.tsx
@@ -28,18 +28,37 @@ interface Agent {
   function: AgentFunction;
   habilidade: Habilidade[];
 }
+interface FunctioAssets {
+  RedIcon: string;
+  BackgroundIcon: string;
+}
 
-const functionImages: Record<AgentFunction, string> = {
-  Controladora:
-    "https://playvalorant.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleController.svg",
-  Sentinela:
-    "https://playvalorant.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleController.svg",
-  Iniciador:
-    "https://playvalorant.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleController.svg",
-  Duelista:
-    "https://playvalorant.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleController.svg",
+const functionImages: Record<AgentFunction, FunctioAssets> = {
+  Controladora: {
+    RedIcon:
+      "https://playvalorant.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleController.svg",
+    BackgroundIcon:
+      "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/8fb7c5a6d20b0ff6dbe9e95064ee5b773652d4a2-529x529.png?auto=format&fit=fill&q=80&w=529",
+  },
+  Sentinela: {
+    RedIcon:
+      "https://playvalorant.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleController.svg",
+    BackgroundIcon:
+      "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/8fb7c5a6d20b0ff6dbe9e95064ee5b773652d4a2-529x529.png?auto=format&fit=fill&q=80&w=529",
+  },
+  Iniciador: {
+    RedIcon:
+      "https://playvalorant.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleController.svg",
+    BackgroundIcon:
+      "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/8fb7c5a6d20b0ff6dbe9e95064ee5b773652d4a2-529x529.png?auto=format&fit=fill&q=80&w=529",
+  },
+  Duelista: {
+    RedIcon:
+      "https://playvalorant.com/_next/static/node_modules/@riotgames/blades-ui/dist/skins/common/assets/roleController.svg",
+    BackgroundIcon:
+      "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/8fb7c5a6d20b0ff6dbe9e95064ee5b773652d4a2-529x529.png?auto=format&fit=fill&q=80&w=529",
+  },
 };
-
 const InternalAgents: React.FC = () => {
   const { agentId } = useParams<{ agentId: string }>();
 
@@ -50,29 +69,30 @@ const InternalAgents: React.FC = () => {
 
   if (!agent)
     return <div className="text-white p-8">Agente não encontrado.</div>;
-
+  const assets = functionImages[agent.function as AgentFunction];
   return (
     <div className="text-white">
       {/* Banner */}
-      <section>
-        <div className="relative">
+      <section className="xl:flex xl:flex-col xl:items-center">
+        <div className="relative w-full">
           <img
             src={agent.internalImage}
             alt={`Banner com o agente ${agent.name}`}
-            className="w-full"
+            className="w-full xl:h-[150vh] object-cover z-20"
           />
           <div className="absolute inset-0 bg-azulmarinho/40"></div>
+          <div className="absolute top-0 left-0 bg-gradient-to-r from-black/80 to-transparent w-1/2 h-full"></div>
         </div>
 
-        <div className="flex flex-col items-center bg-azulmarinho text-left py-8">
-          <div className="flex flex-col w-[90%]">
-            <h1 className="font-tungsten text-white text-[3.6rem] font-bold italic tracking-tight">
+        <div className="flex flex-col items-center bg-azulmarinho text-left py-8 xl:absolute xl:top-[40%] xl:bg-transparent xl:items-start xl:w-[90%]">
+          <div className="flex flex-col w-[90%] xl:w-1/2 xl:gap-5">
+            <h1 className="font-tungsten text-white text-[3.6rem] font-bold italic tracking-tight xl:text-[6rem]">
               {agent.name}
             </h1>
             <span className="text-white text-base font-DINNext font-normal mb-4">
               {agent.description}
             </span>
-            <div className="flex flex-col items-center gap-3 border border-light-red p-4 max-w-max mb-6">
+            <div className="flex flex-col items-center gap-3 outline-1 outline-light-red outline-offset-7 p-4 max-w-max mb-6 bg-[rgb(15,25,35)]">
               <SvgController />
               <div className="flex flex-col items-center uppercase text-[0.6rem]">
                 <p className="font-inter">função</p>
@@ -84,24 +104,38 @@ const InternalAgents: React.FC = () => {
       </section>
 
       {/* Habilidades */}
-      <section className=" flex flex-col items-center p-8 bg-gray-900">
-        <h2 className="text-3xl font-bold mb-6 uppercase font-tungsten text-[3.5rem] leading-12 text-center">
-          Habilidades especiais
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <section className=" flex flex-col items-center p-8 bg-gray-900 xl:flex-row">
+        <div></div>
+        <div className="flex justify-between gap-6 w-full">
           <Tabs
             defaultValue={String(agent.habilidade[0].id)}
-            className="w-[400px]">
-            <TabsList className="bg-transparent! [data-state=active]:bg-transparent!">
-              {agent.habilidade.map((hab) => (
-                <TabsTrigger key={hab.id} value={String(hab.id)}>
-                  <img src={hab.icon} alt={hab.name} className="w-10 h-10" />
-                </TabsTrigger>
-              ))}
-            </TabsList>
+            className="w-full bg-contain bg-center bg-no-repeat pb-5 xl:flex! xl:justify-center xl:flex-row "
+            style={{ backgroundImage: `url(${assets.BackgroundIcon})` }}>
+            <div className="xl:w-1/2 xl:flex xl:flex-col xl:justify-center xl:gap-15">
+              <h2 className="text-3xl font-bold mb-6 uppercase font-tungsten text-[3.5rem] leading-12 text-center xl:text-[6rem]">
+                Habilidades especiais
+              </h2>
+              <TabsList className="bg-transparent! [data-state=active]:bg-transparent! w-full mb-8">
+                {agent.habilidade.map((hab) => (
+                  <TabsTrigger
+                    key={hab.id}
+                    value={String(hab.id)}
+                    className="h-auto">
+                    <img
+                      src={hab.icon}
+                      alt={hab.name}
+                      className="w-12 h-12 xl:w-[4rem] xl:h-[4rem]"
+                    />
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+            </div>
 
             {agent.habilidade.map((hab) => (
-              <TabsContent value={String(hab.id)} key={hab.id}>
+              <TabsContent
+                value={String(hab.id)}
+                key={hab.id}
+                className="flex flex-col items-center gap-5 xl:w-1/2">
                 <video
                   src={hab.video}
                   autoPlay
@@ -110,8 +144,12 @@ const InternalAgents: React.FC = () => {
                   playsInline
                   className="w-full h-auto"
                 />
-                <h2 className="text-xl font-bold mt-2">{hab.name}</h2>
-                <span className="text-sm">{hab.description}</span>
+                <h2 className="text-xl font-bold mt-2 font-tungsten text-[1.75rem] ">
+                  {hab.name}
+                </h2>
+                <span className="font-DINNext text-base leading-7">
+                  {hab.description}
+                </span>
               </TabsContent>
             ))}
           </Tabs>
